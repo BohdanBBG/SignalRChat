f91ba2df76a05b91ad7a4fe9cd1b39db73b88860
Bohdan Berkut <bberk@softserveinc.com>
2023-01-10 16:26:26 +0200

    added vuetify

--- SignalRChat/Web/src/components/HelloWorld.vue
+++ SignalRChat/Web/src/components/HelloWorld.vue
@@ -1,7 +1,13 @@
 <template>
-  <div class="hello">
-    <h1>Hello World!</h1>
-  </div>
+  <v-container>
+    <v-row class="text-center">
+      <v-col class="mb-4">
+        <h1 class="display-2 font-weight-bold mb-3">
+          Hello World!
+        </h1>
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
 
 <script lang="ts">
@@ -12,21 +18,3 @@ export default class HelloWorld extends Vue {
   @Prop() private msg!: string;
 }
 </script>
-
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped lang="scss">
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
-</style>
