ab518f0c65f1cd0d25f1683e9e7712aaaccd40fb
Bohdan Berkut <bberk@softserveinc.com>
2023-01-10 17:08:40 +0200

    custom preset

--- SignalRChat/Web/src/plugins/vuetify.ts
+++ SignalRChat/Web/src/plugins/vuetify.ts
@@ -1,7 +1,9 @@
 import Vue from 'vue';
 import Vuetify from 'vuetify/lib/framework';
+import { VuetifyCustomPreset } from './vuetifyPreset';
 
 Vue.use(Vuetify);
 
 export default new Vuetify({
+    preset: VuetifyCustomPreset
 });
